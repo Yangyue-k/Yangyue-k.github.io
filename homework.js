f0a1a544e43086bae4133c242d33a6146fe6ef85
Yangyue-k <126587845+Yangyue-k@users.noreply.github.com>
2023-05-07 12:12:43 +0800

    Add files via upload

---
+++ homework.js
@@ -0,0 +1,28 @@
+function Hello(){
+  var response = prompt("What is your name?");
+  alert("Hello "+response+",have a good luck!");
+}
+
+function sayHello(){
+  var response = prompt("What is your answer?");
+  if (response=="西安") {
+      alert("恭喜你，答对了！")；
+  } else {
+      alert("很抱歉，答案不正确，请重新输入。");
+}
+
+function sayHello1(){
+  var response = prompt("What is your answer?");
+  if (response=="相思") {
+      alert("恭喜你，答对了！")；
+  } else {
+      alert("很抱歉，答案不正确，请重新输入。");
+}
+
+function sayHello2(){
+  var response = prompt("What is your answer?");
+  if (response=="失眠") {
+      alert("恭喜你，答对了！")；
+  } else {
+      alert("很抱歉，答案不正确，请重新输入。");
+}
