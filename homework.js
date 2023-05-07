131b12c56e3e5e7ba60ede0c8e1e1c3f2eaea00c
Yangyue-k <126587845+Yangyue-k@users.noreply.github.com>
2023-05-07 12:50:55 +0800

    Add files via upload

--- homework.js
+++ homework.js
@@ -1,28 +1,34 @@
 function Hello(){
-  var response = prompt("What is your name?");
-  alert("Hello "+response+",have a good luck!");
-}
+    var response = prompt("What is your name?");
+    alert("Hello "+response+",have a good luck!");
+  }
 
-function sayHello(){
-  var response = prompt("What is your answer?");
-  if (response=="西安") {
-      alert("恭喜你，答对了！");
-  } else {
-      alert("很抱歉，答案不正确，请重新输入。");
-}
+  function sayHello(){
+      var response = prompt("What is your answer?");
+      if (response=="西安") {
+          alert("恭喜你，答对了！");
+      } else {
+          alert("很抱歉，答案不正确，请重新输入。");
+          sayHello()
+      }
+  }
 
-function sayHello1(){
-  var response = prompt("What is your answer?");
-  if (response=="相思") {
-      alert("恭喜你，答对了！");
-  } else {
-      alert("很抱歉，答案不正确，请重新输入。");
-}
+  function sayHello1(){
+    var response = prompt("What is your answer?");
+    if (response=="相思") {
+        alert("恭喜你，答对了！");
+    } else {
+        alert("很抱歉，答案不正确，请重新输入。");
+        sayHello1()
+    }
+  }
 
-function sayHello2(){
-  var response = prompt("What is your answer?");
-  if (response=="失眠") {
-      alert("恭喜你，答对了！");
-  } else {
-      alert("很抱歉，答案不正确，请重新输入。");
-}
+  function sayHello2(){
+    var response = prompt("What is your answer?");
+    if (response=="失眠") {
+        alert("恭喜你，答对了！");
+    } else {
+        alert("很抱歉，答案不正确，请重新输入。");
+        sayHello2()
+    }
+  }
